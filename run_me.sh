b881f2bfc0218b8f868ee92969af544fcb607d1d
letaw <letaw@ohsu.edu>
2019-05-29 13:51:46 -0700

    Removed gitignore files

--- run_me.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for line in $(ls -1 gatk4_json/picard_vcf_*.json); do
-    python3 parse_gatk_json.py --json ${line} --json_type picard_vcf --xml_out /Users/letaw/PycharmProjects/gatk4_xml_prepare/output
-done
